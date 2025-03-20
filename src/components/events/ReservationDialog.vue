552122e5f74f35fbaf8ba60258565c26b2a54c83
/ <sala@contenance.de>
2025-03-20 16:30:52 +0100

    fixed reservation dialog

--- src/components/events/ReservationDialog.vue
+++ src/components/events/ReservationDialog.vue
@@ -1,24 +1,24 @@
 <template>
-<ion-content class="ion-padding">
-    <ion-button color="primary" id="open-modal" expand="block">Reserve</ion-button>
-    <ion-modal ref="modal" trigger="open-modal" @willDismiss="onWillDismiss">
-      <ion-header>
-        <ion-toolbar>
-          <ion-buttons slot="start">
-            <ion-button @click="cancel()">Cancel</ion-button>
-          </ion-buttons>
-          <ion-title>{{event?.name}}</ion-title>
-          <ion-buttons slot="end">
-            <ion-button :strong="true" @click="confirm()">Confirm</ion-button>
-          </ion-buttons>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content class="ion-padding">
-        
-      </ion-content>
-    </ion-modal>
-  </ion-content>
-  </template>
+    <ion-grid class="ion-padding">
+        <ion-button color="primary" id="open-modal" expand="block">Reserve</ion-button>
+        <ion-modal ref="modal" trigger="open-modal" @willDismiss="onWillDismiss">
+        <ion-header>
+            <ion-toolbar>
+            <ion-buttons slot="start">
+                <ion-button @click="cancel()">Cancel</ion-button>
+            </ion-buttons>
+            <ion-title>{{event?.name}}</ion-title>
+            <ion-buttons slot="end">
+                <ion-button :strong="true" @click="confirm()">Confirm</ion-button>
+            </ion-buttons>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+            
+        </ion-content>
+        </ion-modal>
+    </ion-grid>
+</template>
   
   <script lang="ts" setup>
   import {
