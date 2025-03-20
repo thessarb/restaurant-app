6a0abd21fcd0f00eacd9611b04da1a613fbf912a
/ <sala@contenance.de>
2025-03-20 09:00:37 +0100

    switched styyling to scss

--- src/main.ts
+++ src/main.ts
@@ -32,8 +32,8 @@ import "@ionic/vue/css/display.css";
 /* @import '@ionic/vue/css/palettes/dark.class.css'; */
 import "@ionic/vue/css/palettes/dark.system.css";
 
-/* Theme variables */
-import "./theme/variables.css";
+/* Theme styles */
+import "./theme/main.scss";
 const pinia = createPinia();
 const app = createApp(App).use(IonicVue).use(router).use(pinia);
 
